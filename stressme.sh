267b9f4e47284b2970c0239d0fd2e3097ca095eb
Erwan Granger <32554862+erwangranger@users.noreply.github.com>
2021-06-22 22:23:56 +0000

    simple changes to the stressme script

--- stressme.sh
+++ stressme.sh
@@ -5,7 +5,9 @@ echo "starting stress test"
 IMG_PATH=./dataset/images
 URL=""
 
-for f in $(ls ${IMG_PATH})
+echo
+
+for f in $(ls ${IMG_PATH} | grep -E 'png|jpg|jpeg')
 do 
     echo "processing ${IMG_PATH}/${f}"
     ( echo -n '{"image": "'; base64 ${IMG_PATH}/${f}; echo '"}' ) \
