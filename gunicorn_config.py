cede10dc2ba30342b06f53b828f0c05ce5721c84
Erwan Granger <32554862+erwangranger@users.noreply.github.com>
2021-08-04 11:35:29 -0400

    another way

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -3,7 +3,9 @@ import os
 workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 timeout = int(os.environ.get('GUNICORN_TIMEOUT', '240'))
-GUNICORN_CMD_ARGS = "--bind=0.0.0.0:8080" 
+#GUNICORN_CMD_ARGS = "--bind=0.0.0.0:8080" 
+bind = "0.0.0.0:8000"
+
 
 
 forwarded_allow_ips = '*'
